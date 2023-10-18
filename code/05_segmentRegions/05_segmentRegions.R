605e6d5948163cff6758a7121f52a3e680fbf7f0
Cindy Fang <cindyfang70@gmail.com>
2023-10-18 12:21:45 -0400

    working code for looping over all triangulations and computing the alpha shape

--- code/05_segmentRegions/05_segmentRegions.R
+++ code/05_segmentRegions/05_segmentRegions.R
@@ -29,59 +29,43 @@ tri <- readRDS(args[[2]])
 sfe <- readRDS("code/05_segmentRegions/Br2743_Mid_SFE_filt.RDS")
 tris <- readRDS("code/05_segmentRegions/Br2743_Mid_5548-delaunay-lou25.RDS")
 source(here("code","04_delaunay","delaunay.R"))
-tri <- tris[[1]]
-el <- igraph::graph.edgelist(arcs(tri))
-# adj <- get.adjacency(el)
-#dimnames(adj) <- list(V(el), V(el))
 
-comp <- components(el) # components() takes a graph as input, not adjacency matrix
-max.inds <- which.max(comp$csize)
-max.comp.vertices <- groups(comp)[[max.inds]]
-
-tri.comp <- tri
-
-comp.x <- tri$x[max.comp.vertices]
-comp.y <- tri$y[max.comp.vertices]
-
-alpha.shape <- ashape(comp.x, comp.y, alpha=100)
-alpha.shape$delvor.obj$tri.obj$arcs <- as.data.frame(arcs(alpha.shape$delvor.obj$tri.obj))
-#library(Voyager)
-
-alpha.edges <- as.data.frame(alpha.shape$edges)
-from.x <- alpha.edges$x1
-from.y <- alpha.edges$y1
-
-to.x <- alpha.edges$x2
-to.y <- alpha.edges$y2
-
-seg.df <- as.data.frame(cbind(from.x, from.y, to.x,to.y))
-
-# x_min <- min(spatialCoords(sfe)[,1])
-# y_min <- min(spatialCoords(sfe)[,2])
-# scale.df <- data.frame(x=x_min + 100, xend=x_min + 600,
-#                        y=y_min+100, yend=y_min+100, text="500um")
-
-p <- plotGeometry(sfe, type="cellSeg")+
-    geom_segment(data=seg.df, aes(x=from.x,xend = to.x, y=from.y,yend = to.y))
-    #geom_segment(data=scale.df, aes(x=x, xend=xend,y=y,yend=yend))+
-    #geom_text(data=scale.df, aes(label=text, x=xend, y=yend-300))
-
-comps <- list()
+plist <- list()
 for (i in 1:length(tri)){
+    tri <- tris[[i]]
+    g <- igraph::graph.edgelist(arcs(tri)) # build igraph
     
-    # build an igraph object
-    g <- graph.edgelist(arcs(tri))
+    # find biggest component
+    comp <- components(g) # components() takes a graph as input, not adjacency matrix
+    max.inds <- which.max(comp$csize)
+    max.comp.vertices <- igraph::groups(comp)[[max.inds]]
     
-    # find the components
-    comp <- components(g)
+    # compute the alpha shape using the maximal component
+    comp.x <- tri$x[max.comp.vertices]
+    comp.y <- tri$y[max.comp.vertices]
     
-    # which vertices are in each component
-    groups(comp)
+    alpha.shape <- ashape(comp.x, comp.y, alpha=100)
+    alpha.shape$delvor.obj$tri.obj$arcs <- as.data.frame(
+        arcs(alpha.shape$delvor.obj$tri.obj))
     
-    # we only care about components that are sufficiently large. so we want
-    # to identify the "large enough" components and then subset the triangulation
-    # object to contain only those vertices in the component. Then, we can plot them.
+    # plot the alpha shape on the tissue
+    alpha.edges <- as.data.frame(alpha.shape$edges)
+    from.x <- alpha.edges$x1
+    from.y <- alpha.edges$y1
     
+    to.x <- alpha.edges$x2
+    to.y <- alpha.edges$y2
     
-}
+    seg.df <- as.data.frame(cbind(from.x, from.y, to.x,to.y))
+    
 
+
+    p <- plotGeometry(sfe, type="cellSeg")+
+        geom_segment(data=seg.df, aes(x=from.x,xend = to.x, y=from.y,yend = to.y))+
+        geom_segment(data=scale.df, aes(x=x, xend=xend,y=y,yend=yend))+
+        geom_text(data=scale.df, aes(label=text, x=xend, y=yend-300))
+    
+    plist <- list.append(plist, p)
+
+}
+do.call(gridExtra::grid.arrange, plist)
