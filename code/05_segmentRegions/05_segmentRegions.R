b1c977080e743a9782ee3300d5980d8c4d7a3b84
Cindy Fang <cindyfang70@gmail.com>
2023-10-24 15:10:17 -0400

    add code for finding the cells that are inside the alpha shape

--- code/05_segmentRegions/05_segmentRegions.R
+++ code/05_segmentRegions/05_segmentRegions.R
@@ -16,6 +16,7 @@ suppressPackageStartupMessages({
     library(igraph)
     library(rlist)
     library(alphahull)
+    library(sp)
 })
 ###########################################################
 # Find the connected components within the pruned         #
@@ -26,9 +27,9 @@ source(here("code","cindy","04_delaunay","delaunay.R"))
 sfe <- readRDS(args[[1]])
 tris <- readRDS(args[[2]])
 
-#sfe <- readRDS("code/05_segmentRegions/Br2743_Mid_SFE_filt.RDS")
-#tris <- readRDS("code/05_segmentRegions/Br2743_Mid_5548-delaunay-lou25.RDS")
-#source(here("code","04_delaunay","delaunay.R"))
+sfe <- readRDS("Br2743_Mid_SFE_filt.RDS")
+tris <- readRDS("Br2743_Mid_5548-delaunay-lou25.RDS")
+source(here("code","04_delaunay","delaunay.R"))
 
 # compute the coordinates for the scale bar
 x_min <- min(spatialCoords(sfe)[,1])
@@ -36,8 +37,11 @@ y_min <- min(spatialCoords(sfe)[,2])
 scale.df <- data.frame(x=x_min + 100, xend=x_min + 600,
                        y=y_min+100, yend=y_min+100, text="500um")
 plist <- list()
+alpha.shapes <- list()
 for (i in 1:length(tris)){
     tri <- tris[[i]]
+    #clustName <- unique(tri$arcs$clust) # get the name of the cluster
+    clustName <- sprintf("clust%s", i)
     g <- igraph::graph.edgelist(arcs(tri)) # build igraph
     
     # find biggest component
@@ -52,6 +56,13 @@ for (i in 1:length(tris)){
     alpha.shape <- ashape(comp.x, comp.y, alpha=100)
     alpha.shape$delvor.obj$tri.obj$arcs <- as.data.frame(
         arcs(alpha.shape$delvor.obj$tri.obj))
+    
+    # area of the alpha shape
+    ashape <- alpha.shape
+    bds <-ashape$x[alpha.shape$alpha.extremes,]   # matrix of coordinates in ashapebds <- 
+    bds <- rbind(bds, bds[1,])          # close the ring
+    ashape <- Polygon(bds)   
+    print(ashape@area)
     
     # plot the alpha shape on the tissue
     alpha.edges <- as.data.frame(alpha.shape$edges)
@@ -69,15 +80,35 @@ for (i in 1:length(tris)){
     p <- plotSpatialFeature(sfe, "MOBP")+
         geom_segment(data=seg.df, aes(x=from.x,xend = to.x, y=from.y,yend = to.y))+
         geom_segment(data=scale.df, aes(x=x, xend=xend,y=y,yend=yend))+
-        geom_text(data=scale.df, aes(label=text, x=xend, y=yend-300))
+        geom_text(data=scale.df, aes(label=text, x=xend, y=yend-300))+
+        ggtitle(clustName)
     
+    # save the alpha shapes to plot them all on the same tissue
+    seg.df$clust <- clustName # need to change this later to reflect variable cluster names
+    alpha.shapes <- list.append(alpha.shapes, seg.df)
     plist <- list.append(plist, p)
 
 }
+
+all.alpha.shapes <- do.call(rbind, alpha.shapes[c(2,4,6,12)])
+all.alpha.shapes <- as.data.frame(all.alpha.shapes)
 
 fname <- paste(sfe$region_id[[1]], "alphashape", sep="-")
 pdfname <- paste0(fname, ".pdf")
 
 pdf(here("plots", "cindy", "05_segmentRegions", pdfname))
 do.call(gridExtra::grid.arrange, plist)
+plotGeometry(sfe, type="cellSeg")+
+    geom_segment(data=all.alpha.shapes, linewidth=1, aes(x=from.x,xend = to.x, y=from.y,yend = to.y,
+                                            colour=as.factor(clust)))+
+    geom_segment(data=scale.df, aes(x=x, xend=xend,y=y,yend=yend))+
+    geom_text(data=scale.df, aes(label=text, x=xend, y=yend-300))
 dev.off()
+
+# see if the coords are in the shape
+coords <- colGeometries(sfe)$centroid
+bds <- st_as_sf(as.data.frame(bds), coords=c("V1", "V2") )
+sf::st_intersects(coords, st_as_sf(Polygon(ashape)))
+
+sp::over(coords, ashape)
+sf::st_intersection(coords, st_as_sf(ashape))
