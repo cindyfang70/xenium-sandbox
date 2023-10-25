9a7aa14f3a7d5c518d592ea0926c0a2f82b5b534
Cindy Fang <cindyfang70@gmail.com>
2023-10-25 13:34:32 -0400

    function for computing path length

--- code/04_delaunay/04_pruneLocalEdges.R
+++ code/04_delaunay/04_pruneLocalEdges.R
@@ -22,10 +22,40 @@ suppressPackageStartupMessages({
 sfe <- readRDS("Br2743_Mid_SFE_filt.RDS")
 tris <- readRDS("Br2743_Mid_5548-delaunay-lou25.RDS")
 source(here("code","04_delaunay","delaunay.R"))
+tri <- tris[[1]]
+g <- igraph::graph.edgelist(arcs(tri), directed=FALSE)
 
-g <- igraph::graph.edgelist(arcs(tri))
 
-paths <- all_simple_paths(g, 1, V(g))
+v <- 1
+paths <- all_simple_paths(g, v, V(g), cutoff=2)
 
-paths.lengths <- lapply(paths, length)
-length_two_paths <- paths[which(paths.lengths <=2)]
+
+#  2-Order_Mean(Pi) is the mean length of all edges that belong to a path of 
+# two or fewer edges starting from Pi in Gi.
+
+# compute the path lengths:
+computePathLength <- function(tri, path){
+    # compute the length of the path from the first vertex in the given path
+    # to the last
+    edges <- tri$arcs
+    total.length <- 0
+    for (i in 2:length(path)){
+        from <- path[i-1]
+        to <- path[i]
+        
+        # get the edge names, could be either from-to or to-from
+        edge.name <- paste(from, to, sep="-") 
+        edge.name <- c(edge.name, paste(to, from, sep="-"))
+        
+        # get the length of the edge
+        el <- edges[rownames(edges) %in% edge.name,]
+        length <- el$edge.lengths
+        
+        # add the edge length to the total
+        total.length <- total.length + length
+    }
+
+    return(total.length)
+}
+
+computePathLength(tri, paths[[26]])
