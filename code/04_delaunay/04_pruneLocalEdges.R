a1691339281398fe70b5ed18edab6ef9a5f6b861
Cindy Fang <cindyfang70@gmail.com>
2023-10-25 13:52:09 -0400

    compute local distance constraint

--- code/04_delaunay/04_pruneLocalEdges.R
+++ code/04_delaunay/04_pruneLocalEdges.R
@@ -27,7 +27,7 @@ g <- igraph::graph.edgelist(arcs(tri), directed=FALSE)
 
 
 v <- 1
-paths <- all_simple_paths(g, v, V(g), cutoff=2)
+
 
 
 #  2-Order_Mean(Pi) is the mean length of all edges that belong to a path of 
@@ -58,17 +58,9 @@ computePathLength <- function(tri, path){
     return(total.length)
 }
 
-path.edge.lengths <- sum(unlist(lapply(paths, computePathLength, tri=tri)))
-paths.num.edges <- sum(unlist(lapply(paths, length)))
-
-two_order_mean <- path.edge.lengths/paths.num.edges
-
 # Mean_Variation(Pi) is the mean value of all Local_Variation(Qi) for 
 # Qi belonging to a path of two or fewer edges starting at Pi.
 
-# find the nodes belonging to a path of two or fewer edges starting at v
-two.order.neighbours <- unlist(paths)
-two.order.neighbours <- unique(two.order.neighbours[two.order.neighbours!=v])
 
 # Local_Variation(Qi) be the standard deviation of the length of the edges 
 # directly incident to Qi.
@@ -77,10 +69,35 @@ computeLocalVariation <- function(tri, vertex){
     # find the neighbours of the given vertex
     nn <- rbind(edges[which(edges$from ==vertex),],
                 edges[which(edges$to ==vertex),])
-    #print(nn)
     
     # compute the mean of neighbouring edge lengths
     local_variation <- sd(nn$edge.lengths)
     return(local_variation)
 }
 
+
+
+computeLocalEdgeConstraint <- function(tri, g, vertex, beta=2){
+    
+    # find all simple paths of length 2 originating from vertex
+    paths <- all_simple_paths(g, v, V(g), cutoff=2)
+    
+    # find the mean of the edges involved in the paths (two order mean)
+    path.edge.lengths <- sum(unlist(lapply(paths, computePathLength, tri=tri)))
+    paths.num.edges <- sum(unlist(lapply(paths, length)))
+    
+    two_order_mean <- path.edge.lengths/paths.num.edges
+    
+    # find the nodes belonging to a path of two or fewer edges starting at v
+    two.order.neighbours <- unlist(paths)
+    two.order.neighbours <- unique(two.order.neighbours[two.order.neighbours!=v])
+    
+    # compute the mean sd of the edges of the two order neighbours
+    two.order.vars <- lapply(two.order.neighbours, computeLocalVariation, tri=tri)
+    mean_variation <- mean(unlist(two.order.vars))
+    
+    local_distance_constraint <- two_order_mean + beta*mean_variation
+    return(local_distance_constraint)
+}
+
+computeLocalEdgeConstraint(tri, g, v)
