f340904ac73b2785d85537f68ece23a789abfec2
Cindy Fang <cindyfang70@gmail.com>
2023-11-11 17:24:22 -0500

    read from filtered SPE instead to avoid redoing QC

--- code/01_createSCE/readVisium.R
+++ code/01_createSCE/readVisium.R
@@ -16,8 +16,7 @@ suppressPackageStartupMessages({
 # Define the samples for which we want to get the Visium data
 sample_info <- c("Br6471_post", "Br6522_post", "Br8667_mid", "Br2743_mid")
 
-load("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/01_build_spe/spe_raw_final.Rdata")
-
+load("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/01_build_spe/spe_filtered_final.Rdata")
 
 for (i in 1:length(sample_info)){
     sample_id <- sample_info[[i]]
