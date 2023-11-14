6cd84452dee7e47e1df37611abbf3327fc4c67f1
Cindy Fang <cindyfang70@gmail.com>
2023-11-14 14:49:23 -0500

    change input to genesBelowNegProbes.R to use unfiltered SFE instead

--- code/02_qualityControl/sub_genesBelowNegControls.sh
+++ code/02_qualityControl/sub_genesBelowNegControls.sh
@@ -10,4 +10,4 @@ module load conda_R/4.3.x
 
 slide=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
 sample=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
-Rscript code/cindy/02_qualityControl/genesBelowNegControls.R processed-data/cindy/slide-${slide}/${sample}_SFE_filt.RDS
+Rscript code/cindy/02_qualityControl/genesBelowNegControls.R processed-data/cindy/slide-${slide}/${sample}_SFE.RDS
