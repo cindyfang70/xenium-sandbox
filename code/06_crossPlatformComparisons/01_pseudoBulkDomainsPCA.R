3ab1f77f354d17fab481c9f4675cbeff74dd9ff3
Cindy Fang <cindyfang70@gmail.com>
2023-11-15 20:48:22 -0500

    update correlation plotting code to use the BayesSpace Visium domains rather than Banksy

--- code/06_crossPlatformComparisons/01_pseudoBulkDomainsPCA.R
+++ code/06_crossPlatformComparisons/01_pseudoBulkDomainsPCA.R
@@ -34,19 +34,19 @@ spe <- readRDS(args[[2]])
 
 # pseudobulk across the domain labels
 sfe.summed <- aggregateAcrossCells(sfe, id=colData(sfe)[,c("clust_M1_lam0.9_k50_res1.2")])
-spe.summed <- aggregateAcrossCells(spe, id=colData(spe)[,c("clust_M1_lam0.9_k50_res1.2")])
+spe.summed <- aggregateAcrossCells(spe, id=colData(spe)[,c("BayesSpace_harmony_06")])
 
 # convert the SFE to SPE so that the two datasets can be combined
 sfe.summed <- SFEtoSPE(sfe.summed)
 
 # subset the visium data to the same genes that are in xenium
 spe.summed <- spe.summed[which(rowData(spe.summed)$gene_name %in% rownames(sfe.summed)),]
-# # remove the imgData
-# imgData(spe.summed) <- NULL
+# rename the coldata column denoting the sample to match what's in the sfe
+spe.summed$sample_id <- spe.summed$subject_position 
 
 # Now subset the colData for both to have the same
 colData(sfe.summed) <- colData(sfe.summed)[c("sample_id", "clust_M1_lam0.9_k50_res1.2")]
-colData(spe.summed) <- colData(spe.summed)[c("sample_id", "clust_M1_lam0.9_k50_res1.2")]
+colData(spe.summed) <- colData(spe.summed)[c("sample_id", "BayesSpace_harmony_06")]
 
 # rename the spatial coordinates to be the same 
 col <- spatialCoords(spe.summed)[,1]
@@ -71,7 +71,7 @@ new.sfe$platform <- "Xenium"
 new.spe$platform <- "Visium"
 
 new.sfe$clust <- paste0(new.sfe$platform, new.sfe$clust_M1_lam0.9_k50_res1.2)
-new.spe$clust <- paste0(new.spe$platform, new.spe$clust_M1_lam0.9_k50_res1.2)
+new.spe$clust <- paste0(new.spe$platform, new.spe$BayesSpace_harmony_06)
 spes.all <- cbind(new.sfe, new.spe)
 
 #spes.all <- logNormCounts(spes.all)
@@ -85,7 +85,7 @@ ComplexHeatmap::Heatmap(m)
 m <- m[!grepl("Visium", rownames(m)),!grepl("Xenium", colnames(m))]
 
 
-pdfname <- here("plots", "cindy", "06_ss", 
+pdfname <- here("plots", "cindy", "06_crossPlatformComparisons0", 
                 paste0(unique(sfe$region_id), 
                 "Visium-Xenium-Correlation-Heatmap.pdf"))
 
