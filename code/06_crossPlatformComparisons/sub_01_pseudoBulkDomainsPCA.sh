682efaa86862ab4a7266636f7a667633054f3912
Cindy Fang <cindyfang70@gmail.com>
2023-11-17 15:21:55 -0500

    modify submission script to provide which clusters to compute the correlations for

--- code/06_crossPlatformComparisons/sub_01_pseudoBulkDomainsPCA.sh
+++ code/06_crossPlatformComparisons/sub_01_pseudoBulkDomainsPCA.sh
@@ -11,4 +11,4 @@ sample=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $c
 
 module load conda_R/4.3.x
 
-Rscript code/cindy/06_crossPlatformComparisons/01_pseudoBulkDomainsPCA.R processed-data/cindy/slide-${slide}/${sample}_SFE_filt.RDS processed-data/cindy/visium/$sample-Visium-SPE.RDS
+Rscript code/cindy/06_crossPlatformComparisons/01_pseudoBulkDomainsPCA.R processed-data/cindy/slide-${slide}/${sample}_SFE_filt-with-banksy-domains.RDS processed-data/cindy/visium/$sample-Visium-SPE.RDS clust_M1_lam0.9_k50_res0.4 BayesSpace_harmony_09
