bed2883e64978ae1b4aaa17b47597c80b2b6b9ae
Cindy Fang <cindyfang70@gmail.com>
2023-11-27 13:18:20 -0500

    plot boxplots of NMF values for each layer

--- code/NMF/exploratory_visium_nmf.R
+++ code/NMF/exploratory_visium_nmf.R
@@ -6,7 +6,7 @@ library(projectR)
 library(spatialLIBD)
 library(escheR)
 library(here)
-
+library(tidyverse)
 # Try using RcppML::nmf for non-negative matrix factorization to compute a feature
 # factor matrix (Amplitude matrix in scCoGAPS terminology) from the manually
 # annotated visium dataset. Then, use projectR to project the Xenium datasets
@@ -18,22 +18,21 @@ vis_anno <- fetch_data(type = "spe", eh = ehub)
 vis_anno <- vis_anno[,which(vis_anno$sample_id == 151673)]
 # use layer_guess_reordered as the manual annotations
 
-# read in the Xenium data to project
-x <- readRDS("processed-data/cindy/slide-5434/Br6471_Post_SFE_filt.RDS")
-
 # NMF on the annotated visium data, try with just counts first but might
 # need to use lognormcounts
 k <- 10
 model <- RcppML::nmf(counts(vis_anno), k = k, seed=1237)
 patterns <- t(model$h)
+#patterns <- model$w
 
+colnames(patterns) <- paste0("NMF", 1:dim(patterns)[[2]])
 colData(vis_anno) <- cbind(colData(vis_anno), patterns)
 
 plist <- list()
-pdf(here("plots", "NMF", sprintf("visium-raw-counts-nmf-k$s.pdf", k)),
+pdf(here("plots", "NMF", sprintf("visium-raw-counts-h-matrix-nmf-k%s.pdf", k)),
     height=20, width=20)
 for (i in 1:dim(patterns)[[2]]){
-    patternName <- paste0("V", i)
+    patternName <- paste0("NMF", i)
     p <- make_escheR(vis_anno) |>
         add_ground("layer_guess_reordered") |>
         add_fill(patternName) +
@@ -46,7 +45,25 @@ for (i in 1:dim(patterns)[[2]]){
 plist <- rlist::list.append(plist, pl)
 
 do.call(gridExtra::grid.arrange, c(plist,ncol=3))
+
+
+cor.mat <- colData(vis_anno)[,c(paste0("NMF", 1:dim(patterns)[[2]]), 
+                                "layer_guess_reordered")] %>%
+    as.data.frame()%>%
+    pivot_longer(cols=starts_with("NMF"))
+ggplot(as.data.frame(cor.mat), aes(x=layer_guess_reordered, y=value, 
+                                   fill=layer_guess_reordered))+
+    geom_boxplot()+
+    facet_wrap(~name, scales="free")+
+    scale_y_log10()+
+    theme(legend.position="none")+
+    theme_minimal()
 dev.off()
+
+
+    
+
+
 # 
 # recon <- model$w %*% model$h
 # 
