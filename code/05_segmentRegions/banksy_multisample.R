adff4a7b99ea44dbc6a3a226888b55c797a5aa75
Cindy Fang <cindyfang70@gmail.com>
2023-11-29 09:37:06 -0500

    make banksy multisample script handle all 6 regions together and add job sub script

--- code/05_segmentRegions/banksy_multisample.R
+++ code/05_segmentRegions/banksy_multisample.R
@@ -6,47 +6,45 @@ library(Banksy)
 # https://github.com/prabhakarlab/Banksy/blob/bioc/vignettes/multi-sample.Rmd
 
 source(here("code","01_createSCE","xenium_helpers.R"))
-    
-sfe.paths1 <- system("ls processed-data/cindy/slide-5434", intern=TRUE)
-sfe.paths1 <- sfe.paths1[grepl("*filt.RDS", sfe.paths1)]
-sfe.paths1 <- paste0("processed-data/cindy/slide-5434/", sfe.paths1)
+
+# sfe.paths1 <- system("ls processed-data/cindy/slide-5434", intern=TRUE)
+# sfe.paths1 <- sfe.paths1[grepl("*filt.RDS", sfe.paths1)]
+# sfe.paths1 <- paste0("processed-data/cindy/slide-5434/", sfe.paths1)
+
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+# get the SFE paths
+sfe.paths <- args[1:6]
+
+# get the Banksy params
+lambda <- args[[7]]
+k_geom <- args[[8]]
+res <- args[[9]]
 
 # read in the individual sfes and 
-#aname <- "normcounts"
-spe_list <- lapply(sfe.paths1, function(x){
+aname <- "normcounts"
+spe_list <- lapply(sfe.paths, function(x){
     x <- readRDS(x)
     x <- SFEtoSPE(x) # note: this function only keeps counts assay
-    #assay(x, aname) <- normalizeCounts(x, log = FALSE)
-    x <- scry::nullResiduals(x,
-                             assay="counts",
-                             fam="poisson",
-                             type="pearson") # try GLM PCA
+    assay(x, aname) <- normalizeCounts(x, log = FALSE)
     return(x) # return SPE for easier downstream manipulations
 })
 
-
-lambda <- 0.9
 use_agf <- FALSE
-k_geom <- 6
 
 # Compute neighbourhood matrices individually
 compute_agf <- FALSE
 spe_list <- lapply(spe_list, computeBanksy, 
-                   assay_name = "poisson_pearson_residuals", 
+                   assay_name = aname,
                    compute_agf = compute_agf, 
                    k_geom = k_geom)
 # cbind the sfes together
 spe_joint <- do.call(cbind, spe_list) 
 
-assay(spe_joint, "joint_poisson_pearson_residuals") <- 
-    assay(scry::nullResiduals(spe_joint, assay="counts",
-                           fam="poisson",
-                           type="pearson"), "poisson_pearson_residuals")
 
 # PCA 
 spe_joint <- runBanksyPCA(spe_joint, use_agf = use_agf, lambda = lambda,
                     group = "region_id",
-                    assay_name="joint_poisson_pearson_residuals",
                     seed = 0858)
 
 # UMAP
@@ -55,7 +53,7 @@ spe_joint <- runBanksyUMAP(spe_joint, use_agf = use_agf,
                           seed = 0922)
 
 # Cluster
-res <- 0.23
+
 spe_joint <- clusterBanksy(spe_joint, use_agf = use_agf, 
                            lambda = lambda, resolution = res, seed = 0923)
 
@@ -63,14 +61,11 @@ cnm <- sprintf("clust_M0_lam%s_k50_res%s", lambda,
               res)
 
 
-# colnames(colData(spe_joint))[grepl(cnm, colnames(colData(spe_joint)))] <- 
-#     paste0("glmpca", cnm)
-
-# cnm <- paste0("glmpca", cnm)
-
 # Split the samples back up into their own SPEs
 spe_list <- lapply(unique(spe_joint$region_id), function(x) 
     spe_joint[, spe_joint$region_id == x])
+
+print(length(spe_list))
 
 plist <- list()
 for (i in seq_along(spe_list)){
@@ -82,35 +77,42 @@ for (i in seq_along(spe_list)){
     plist[[i]] <- p
 }
 
+pdf(here("plots", "cindy", "05_segmentRegions", "banksy",
+         sprintf("multisample-banksy-plots-res%s.pdf", res)), height=15, width=20)
+do.call(gridExtra::grid.arrange, c(plist, ncol=2))
+dev.off()
+
+saveRDS(spe_joint, here("processed-data", "cindy", 
+                        "all-samples-spe-with-banksy.RDS"))
 
 ###############################################
-set.seed(1015)
-spe_joint <- scry::nullResiduals(spe_joint, assay="counts", fam="poisson", type="pearson")
-spe_joint <- scater::runPCA(spe_joint, ncomponents=50, 
-                            ntop = 1000,
-                            exprs_values = "poisson_pearson_residuals",
-                            scale = TRUE, name = "GLM-PCA",
-                            BSPARAM = BiocSingular::RandomParam())
-
-spe_joint <- scater::runUMAP(spe_joint, dimred="GLM-PCA")
-
-pdf(here("plots", "05_segmentRegions", "banksy", 
-         "multisample-slide-5434.pdf"), height=15, width=20)
-do.call(gridExtra::grid.arrange, c(plist, ncol=2))
-
-
-plotReducedDim(spe_joint,ncomponents=4, colour_by="region_id", dimred="GLM-PCA")
-
-plotUMAP(spe_joint, colour_by="region_id") + plotUMAP(spe_joint, colour_by=cnm)
-
-
-spe_joint <- schex::make_hexbin(spe_joint, nbins = 40, 
-                           dimension_reduction = "UMAP", use_dims=c(1,2))
-schex::plot_hexbin_meta(spe_joint, col="region_id", action="majority")
-# schex::plot_hexbin_meta_plus(spe_joint,
-#                          col1=cnm,
-#                          col2="region_id", action="prop")
-schex::plot_hexbin_meta(spe_joint, col="total", action="median")
-schex::plot_hexbin_meta(spe_joint, col="subsets_any_neg_sum", action="median")
-schex::plot_hexbin_meta(spe_joint, col="nucleus_area", action="median")
-dev.off()
+# set.seed(1015)
+# spe_joint <- scry::nullResiduals(spe_joint, assay="counts", fam="poisson", type="pearson")
+# spe_joint <- scater::runPCA(spe_joint, ncomponents=50, 
+#                             ntop = 1000,
+#                             exprs_values = "poisson_pearson_residuals",
+#                             scale = TRUE, name = "GLM-PCA",
+#                             BSPARAM = BiocSingular::RandomParam())
+# 
+# spe_joint <- scater::runUMAP(spe_joint, dimred="GLM-PCA")
+# 
+# pdf(here("plots", "05_segmentRegions", "banksy", 
+#          "multisample-slide-5434.pdf"), height=15, width=20)
+# do.call(gridExtra::grid.arrange, c(plist, ncol=2))
+# 
+# 
+# plotReducedDim(spe_joint,ncomponents=4, colour_by="region_id", dimred="GLM-PCA")
+# 
+# plotUMAP(spe_joint, colour_by="region_id") + plotUMAP(spe_joint, colour_by=cnm)
+# 
+# 
+# spe_joint <- schex::make_hexbin(spe_joint, nbins = 40, 
+#                            dimension_reduction = "UMAP", use_dims=c(1,2))
+# schex::plot_hexbin_meta(spe_joint, col="region_id", action="majority")
+# # schex::plot_hexbin_meta_plus(spe_joint,
+# #                          col1=cnm,
+# #                          col2="region_id", action="prop")
+# schex::plot_hexbin_meta(spe_joint, col="total", action="median")
+# schex::plot_hexbin_meta(spe_joint, col="subsets_any_neg_sum", action="median")
+# schex::plot_hexbin_meta(spe_joint, col="nucleus_area", action="median")
+# dev.off()
