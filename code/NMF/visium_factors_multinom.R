d21d3339e8d961fd9d2a2621429f67b96042573a
Cindy Fang <cindyfang70@gmail.com>
2023-11-29 09:37:54 -0500

    set na.option=na.exclude so that predictions from the model will be same length as design matrix

--- code/NMF/visium_factors_multinom.R
+++ code/NMF/visium_factors_multinom.R
@@ -16,7 +16,7 @@ library(nnet)
 # https://www.dropbox.com/scl/fi/4m2y8flc1d06z6prkjnjy/shared_example_glm.r?rlkey=34t4q0ruyjq42uldmnlmuxk0n&dl=0
 
 # read in the model
-k <- 25
+k <- 20
 model <- readRDS(here("processed-data", "cindy", "NMF", sprintf("visium-nmf-model-k%s", k)))
 factors <- t(model$h)
 colnames(factors) <- paste0("NMF", 1:dim(factors)[[2]])
@@ -24,13 +24,13 @@ colnames(factors) <- paste0("NMF", 1:dim(factors)[[2]])
 # get the manually annotated Visium data
 ehub <- ExperimentHub::ExperimentHub()
 vis_anno <- fetch_data(type = "spe", eh = ehub)
-vis_anno <- vis_anno[,which(vis_anno$sample_id == 151673)]
 
 # create the design matrix
 design <- cbind(colData(vis_anno)["layer_guess_reordered"], factors)
 
 # fit the multinomial model
-mod <-  multinom(layer_guess_reordered ~ ., data = design)
+mod <-  multinom(layer_guess_reordered ~ ., data = design,
+                 na.action=na.exclude)
 
 # predict on the same data
 p.fit <- predict(mod, predictors=design, type='probs') 
@@ -43,4 +43,8 @@ pred = unlist(lapply(1:nrow(p.fit), function(xx){
 labs <- design$layer_guess_reordered[!is.na(design$layer_guess_reordered)]
 acc = mean(pred==labs,na.rm=T)
 acc # k=20: 0.7441152, k=15: 0.712545, k=25: 0.7493769
+#k=20 all samples: 0.7703311
+
+saveRDS(mod, here("processed-data", "cindy", "NMF", sprintf("visium-nmf-k%s-multinom-model.RDS",k)))
+
                  
