fed32b642d7a31fadcbf068cc8f61dcacff1828c
Cindy Fang <cindyfang70@gmail.com>
2023-12-19 16:35:36 -0500

    slurm submission script

---
+++ code/NMF/sub_exploratory_visium_nmf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=NMF_SNRNA
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=64G
+module load conda_R/4.3.x
+
+Rscript code/cindy/NMF/exploratory_visium_nmf.R spe 30
