01d0a9deb23d2c1bac375fb32907fbf0dbf128ca
Cindy Fang <cindyfang70@gmail.com>
2024-02-20 11:54:25 -0500

    filter the snRNA-seq to only the nuclei that are not spatially ambiguous

--- code/NMF/01_exploratory_visium_nmf.R
+++ code/NMF/01_exploratory_visium_nmf.R
@@ -18,31 +18,52 @@ library(ggforce)
 args <- commandArgs(trailingOnly = TRUE)
 model_type <- args[[1]]
 
+# get the annotated source data
+ehub <- ExperimentHub::ExperimentHub()
 
 if(model_type == "manual_annot"){ # use manual annotations
     layer_labs <- "layer_guess_reordered"
     data_type <- "spe"
+    vis_anno <- fetch_data(type = data_type, eh = ehub)
 }else if(model_type == "bayesspace"){
     layer_labs <- "BayesSpace_harmony_09"
     data_type <- "spatialDLPFC_Visium"
-}
+    vis_anno <- fetch_data(type = data_type, eh = ehub)
+}else if (model_type=="snRNA-seq"){
+    load(here("processed-data", "cindy", "snRNA-seq", "sce_DLPFC.Rdata"))
+    
+    # drop the nuclei that were deemed spatially ambiguous
+    sce.filt <- sce[,which(!is.na(sce$cellType_layer))]
+    sce.filt <- sce.filt[,which(!grepl("ambig", sce.filt$cellType_layer))]
+    
 
-# get the manually annotated visium data
-ehub <- ExperimentHub::ExperimentHub()
-vis_anno <- fetch_data(type = data_type, eh = ehub)
+    vis_anno <- sce.filt # reassign to the same variable so following code doesn't need to be modified
+    
+    stopifnot(dim(vis_anno)[[2]] == 54394) # make sure the dims match the psychencode paper
+    rm(sce)
+    rm(sce.filt)
+    layer_labs <- "layer_annotation"
+    vis_anno$sample_id <- vis_anno$Sample
+    
+}
 
 # NMF on the annotated visium data
 k <- as.numeric(args[[2]])
 A <- logcounts(vis_anno) # using logcounts because there are multiple datasets
-#model <- RcppML::nmf(A, k = k, seed=1237)
-model <- readRDS(here("processed-data", "cindy", "NMF", model_type,
-                      sprintf("%s-visium-nmf-model-k%s", model_type, k)))
+
+# trying out a counts-based approach instead:
+#A <- scuttle::normalizeCounts(vis_anno, transform="none")
+
+model <- RcppML::nmf(A, k = k, seed=1237)
+# model <- readRDS(here("processed-data", "cindy", "NMF", model_type,
+#                       sprintf("%s-nmf-model-k%s-counts-based.RDS", model_type, k)))
+
 patterns <- t(model$h) # these are the factors
 
 rownames(model$w) <- rowData(vis_anno)$gene_name
 
-saveRDS(model, here("processed-data", "cindy", "NMF", model_type, 
-                    sprintf("%s-visium-nmf-model-k%s.RDS", model_type, k)))
+saveRDS(model, here("processed-data", "cindy", "NMF", model_type,
+                     sprintf("%s-nmf-model-k%s.RDS", model_type, k)))
 
 colnames(patterns) <- paste0("NMF", 1:k)
 colData(vis_anno) <- cbind(colData(vis_anno), patterns)
@@ -85,6 +106,9 @@ brains <- unique(vis_anno$sample_id)
 
 # Compute correlation between NMF factors and layer identities 
 # colnames(patterns) <- paste0("NMF", 1:k)
+patternSums <- colSums(patterns)
+patterns <- patterns[,which(patternSums != 0)]
+
 labels <- unique(colData(vis_anno)[[layer_labs]])
 n_labels <-length(labels)
 factors.ind <- matrix(,nrow=dim(vis_anno)[[2]], ncol=n_labels)
@@ -118,52 +142,22 @@ brs.ind <- as.data.frame(brs.ind)
 
 
 brs.cor.mat <- cbind(patterns, brs.ind)
-print(colnames(brs.cor.mat))
 
 brs.M <- cor(brs.cor.mat, use="complete.obs")
+brs.M <- brs.M[grepl("NMF", rownames(brs.M)), !grepl("NMF", colnames(brs.M))]
 print(head(brs.M))
-if (data_type=="spe"){
-    brs.M <- brs.M[grepl("NMF", rownames(brs.M)), grepl("^[[:digit:]]+$", colnames(brs.M))]
-}else if (data_type=="spatialDLPFC_Visium"){
-    brs.M <- brs.M[grepl("NMF", rownames(brs.M)), grepl("^Br", colnames(brs.M))]
-}
-
-print(brs)
-print(head(brs.M))
-
-# pseudobulk to get an expression profile for each domain label and compute the
-# correlation between each NMF factor and the domain expression profile.
-
-summed <- scuttle::aggregateAcrossCells(vis_anno, 
-                            ids=colData(vis_anno)[c(layer_labs, "sample_id")])
-summed$layer_donor <- paste(colData(summed)[[layer_labs]], 
-                            summed$sample_id, sep="_")
-
-expr_profiles <- as.data.frame(as.matrix(counts(summed)))
-colnames(expr_profiles) <- summed$layer_donor
-
-expr_prof_cor_mat <- cbind(patterns, expr_profiles)
-exprs.M <- cor(expr_prof_cor_mat, use="complete.obs")
-
-exprs.M <- exprs.M[grepl("NMF", rownames(exprs.M)), 
-                   !(grepl("NMF", colnames(exprs.M)))]
-print(head(exprs.M))
-
+print(any(is.na(brs.M)))
+print(head(M))
+print(any(is.na(M)))
 
 col <- circlize::colorRamp2(seq(-1, 1, length = 3), c("blue", "#EEEEEE", "red"))
-fname <- sprintf("%s_NMF_layer_corr_plot_k%s.pdf", model_type, k)
-pdf(here("plots", "cindy", "NMF", model_type, fname), height=30, width=30)
+fname <- sprintf("%s_NMF_layer_corr_plot_k%s-counts-based.pdf", model_type, k)
+pdf(here("plots", "cindy", "NMF", model_type, fname), height=30, width=60)
 ComplexHeatmap::Heatmap(t(as.matrix(M)), col=col,
                         row_names_gp = grid::gpar(fontsize = 30),
                         column_names_gp = grid::gpar(fontsize = 25),
                         width = ncol(t(as.matrix(M)))*unit(6.5, "mm"), 
                         height = nrow(t(as.matrix(M)))*unit(20, "mm"))
-
-ComplexHeatmap::Heatmap(t(as.matrix(exprs.M)), col=col,
-                        row_names_gp = grid::gpar(fontsize = 30),
-                        column_names_gp = grid::gpar(fontsize = 25),
-                        width = ncol(t(as.matrix(exprs.M)))*unit(6.5, "mm"), 
-                        height = nrow(t(as.matrix(exprs.M)))*unit(20, "mm"))
 ComplexHeatmap::Heatmap(t(as.matrix(brs.M)), col=col,
                         row_names_gp = grid::gpar(fontsize = 30),
                         column_names_gp = grid::gpar(fontsize = 25),
@@ -171,7 +165,7 @@ ComplexHeatmap::Heatmap(t(as.matrix(brs.M)), col=col,
                         height = nrow(t(as.matrix(brs.M)))*unit(20, "mm"))
 dev.off()
 
-cors <- list(layer_cor=M, exprs_cor = exprs.M, sample_cor=brs.M)
+cors <- list(layer_cor=M, sample_cor=brs.M)
 saveRDS(cors, here("processed-data", "cindy", "NMF", model_type,
                     sprintf("%s-nmf-correlations-k%s.RDS", model_type, k)))
 
